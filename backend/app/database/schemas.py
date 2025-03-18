619256d3248cd4defe935c1cad8fbf769e2d8151
michale-law <yourlaw21@gmail.com>
2025-03-18 11:03:50 -0500

    Revert "Initial commit for BackTrackIT branch"

--- backend/app/database/schemas.py
+++ backend/app/database/schemas.py
@@ -1,9 +1,6 @@
 from pydantic import BaseModel
 
 class InventoryItemCreate(BaseModel):
-    id:int
     name: str| None = None
     quantity: int| None = None
     price: float | None = None
-    class Config:
-        orm_mode = True
